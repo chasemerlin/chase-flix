b298d65c5d336f912c2673e4c89a6e9c5f5485bd
Chase Merlin <chasemerlin@gmail.com>
2013-06-03 13:30:45 -0400

    add video show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: 'Family Guy', description: 'Family Guy is an American adult animated sitcom created by Seth MacFarlane for the Fox Broadcasting Company.', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '')
-Video.create(title: 'Futurama', description: 'Futurama is an animated science fiction sitcom created by Matt Groening and developed by Groening and David X. Cohen for the Fox Broadcasting Company', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '')
+Video.create(title: 'Family Guy', description: 'Family Guy is an American adult animated sitcom created by Seth MacFarlane for the Fox Broadcasting Company.', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/monk_large.jpg')
+Video.create(title: 'Futurama', description: 'Futurama is an animated science fiction sitcom created by Matt Groening and developed by Groening and David X. Cohen for the Fox Broadcasting Company', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/monk_large.jpg')
 Video.create(title: 'Monk', description: 'Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
-Video.create(title: 'South Park', description: 'South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network.', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '')
+Video.create(title: 'South Park', description: 'South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network.', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg')
