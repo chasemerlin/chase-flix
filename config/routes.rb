f4d3d40b69037f1c4678a46f9bed8a9ddfa2e7e7
Chase Merlin <chasemerlin@gmail.com>
2013-06-14 16:35:19 -0400

    add users, sessions, authentication, and hide routes if not signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-
   get '/home', to: 'videos#index'
+  get '/sign_in', to: 'sessions#new'
+  get '/sign_out', to: 'sessions#destroy'
+
+  root to: 'pages#front'
+  get '/register', to: 'users#new'
 
   resources :videos, only: [:index, :show] do
   	collection do
   		post 'search'
   	end
   end
+  
   resources :categories, only: [:show]
+
+  resources :users, only: [:create]
+
+  resources :sessions, only: [:create]
 end
